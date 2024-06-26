d5d3c2a5aa7630b3d9e41240246b94799d5c890b
yasuda <taiki.yasuda@g.dreamcareer.co.jp>
2024-06-26 16:43:29 +0900

    コメント追加

--- ci_cd_test/src/hooks/useCounter.js
+++ ci_cd_test/src/hooks/useCounter.js
@@ -3,5 +3,5 @@ import { useState, useCallback } from "react";
 export const useCounter = () => {
   const [count, setCount] = useState(0);
   const addCount = useCallback(() => setCount((v) => v + 1), []);
-  return { count, addCount };
+  return { count, addCount };//sds
 };
